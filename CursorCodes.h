fd98f5db8031bd370e6c856c996f0ba39fa42d32
TimOnTheGo <timfos@ilve.se>
2016-05-01 14:10:21 +0200

    Added Cursorcodes

---
+++ CursorCodes.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Logic
+{
+
+	enum CursorCode
+	{
+
+		CursorCode_NONE = 0,
+		CursorCode_POINT = 1,
+		CursorCode_HAND = 2,
+		CursorCode_LOAD = 3,
+		CursorCode_CARET = 4,
+		CursorCode_NA
+
+	};
+}
