f709710e86f72c95869b1d9de8c6105378e88a5a
TimOnTheGo <timfos@ilve.se>
2016-12-05 17:56:44 +0100

    Revert "Revert "Added clocktasks(Doesn't work)""
    This reverts commit 9b90047ed0caa9af06eef58e9bc22b6989d6bea7.

--- TaskObject.h
+++ TaskObject.h
@@ -30,11 +30,13 @@ namespace Resources
 			CrossHandlers::CelestialStack<float>* GetFParams() const;
 			CrossHandlers::CelestialStack<std::string>* GetSParams() const;
 
+			unsigned int GetTime() const;
+			unsigned int GetFireTime() const;
 			unsigned int GetScript() const;
 			TaskClass GetType() const;
 
 			void Queue(TaskClass type, unsigned int fireTime);
-			bool Time(unsigned int time);
+			void SetTime(unsigned int time);
 			void Kill();
 			bool ShouldKill() const;
 
